1d92d16e713a172f5270cc505711422ed84fe364
vicspidy <vicspidy@gmail.com>
2018-03-17 14:45:51 +0530

    Add files via upload

--- src/projman/common.py
+++ src/projman/common.py
@@ -1,10 +1,13 @@
 import os
 import yaml
 import glob
-import shutil
+import sys
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 __all__ = ['get_yaml_config', 'get_available_configs', 'get_available_types', 'get_project_location', 'AVAILABLE_TYPES',
-           'AVAILABLE_CONFIGS', 'PROJECT_LOCATION']
+           'AVAILABLE_CONFIGS', 'PROJECT_LOCATION', 'show_types', 'display_structre']
 
 KNOWN_TYPES = ['maya', 'houdini', 'nuke', 'photoshop', 'blender']
 AVAILABLE_TYPES = set()
@@ -20,41 +23,64 @@ def get_yaml_config():
                 for type in ('*.yml', '*.yaml'):
                     files_found = glob.glob(type)
                     for file in files_found:
-                        file_desc = open(file, 'r')
-                        data = yaml.load(file_desc)
-                        file_desc.close()
-                        yaml_configs.extend(data)
+                        try:
+                            file_desc = open(os.path.join(path, file), 'r')
+                            data = yaml.load(file_desc)
+                            file_desc.close()
+                            yaml_configs.extend(data)
+                        except yaml.YAMLError, exc:
+                            sys.stderr.write('Error found in file: {0}'.format(file_desc.name))
+                            sys.exit(4)
 
+        # pp.pprint(yaml_configs)
         return yaml_configs
     else:
         return None
 
 def _get_available_config():
     for config in get_yaml_config():
-            for v in config.values():
-                if isinstance(v, dict):
-                    for type, config in v.iteritems():
-                        if type in KNOWN_TYPES:
-                            AVAILABLE_TYPES.add(type)
-                            temp_dict = dict()
-                            temp_dict[type] = config
-                            AVAILABLE_CONFIGS[type] = temp_dict
+        if isinstance(config, dict):
+            if config.get('value'):
+                for key, val in config['value'].iteritems():
+                    if key not in AVAILABLE_TYPES:
+                        AVAILABLE_TYPES.add(key)
+                        AVAILABLE_CONFIGS[key] = config
+                    break
 
 def get_available_configs():
     if not AVAILABLE_CONFIGS:
         _get_available_config()
-        return AVAILABLE_CONFIGS
+    return AVAILABLE_CONFIGS
 
 def get_available_types():
     if not AVAILABLE_TYPES:
         _get_available_config()
         return AVAILABLE_TYPES
 
-def get_project_location():
-    if os.environ.get('PROJMAN_LOCATION'):
-        return os.environ['PROJMAN_LOCATION']
+def get_project_location(args):
+    global PROJECT_LOCATION
+    if args.path:
+        PROJECT_LOCATION = args.path
+        return PROJECT_LOCATION
+    elif os.environ.get('PROJMAN_LOCATION'):
+        PROJECT_LOCATION = os.environ['PROJMAN_LOCATION']
+        return PROJECT_LOCATION
+    else:
+        user_home = os.path.expanduser('~' + os.environ['USERNAME'])
+        project_location = os.path.join(user_home, 'projman', 'projects')
+        if not os.path.exists(project_location):
+            os.makedirs(project_location)
 
+        PROJECT_LOCATION = project_location
+        return PROJECT_LOCATION
 
+def show_types(args):
+    print 'Types of projects you can create..'
+    for type_of_project in AVAILABLE_TYPES:
+        print '\t' + '-' + type_of_project
 
-PROJECT_LOCATION = get_project_location()
+def display_structre(args):
+    for type, config in AVAILABLE_CONFIGS.iteritems():
+        pp.pprint(config)
+
 get_available_configs()
