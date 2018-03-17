83344d7f96d9d5c16fa9e49cc15157485dc4c0f9
vicspidy <vicspidy@gmail.com>
2018-03-17 14:47:13 +0530

    Add files via upload

--- src/projman/list_projects.py
+++ src/projman/list_projects.py
@@ -3,13 +3,16 @@ import common
 
 __all__ = ['list_projects']
 
-def list_projects(type=None):
+def list_projects(args):
+    if args.type:
+        if args.type not in common.AVAILABLE_TYPES:
+            print 'Invalid type of project given: {0}'.format(args.type)
+            return
     for project in os.listdir(common.PROJECT_LOCATION):
         current_project = os.path.join(common.PROJECT_LOCATION, project)
         if os.path.isdir(current_project):
-            if type:
-                if type in common.AVAILABLE_TYPES:
-                    if os.path.exists(os.path.join(current_project, type)):
-                        print project
+            if args.type:
+                if os.path.exists(os.path.join(current_project, args.type)):
+                    print project
             else:
                 print project
