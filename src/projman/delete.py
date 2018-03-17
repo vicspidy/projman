7fb24eccd74d81d8d6ac1c167ec6f3c81086cba3
vicspidy <vicspidy@gmail.com>
2018-03-17 14:46:59 +0530

    Add files via upload

--- src/projman/delete.py
+++ src/projman/delete.py
@@ -5,13 +5,24 @@ import common
 
 __all__ = ['delete_projects']
 
-def delete_projects(type=None):
-    for project in os.listdir(common.PROJECT_LOCATION):
-        current_project = os.path.join(common.PROJECT_LOCATION, project)
-        if os.path.isdir(current_project):
-            if type:
-                if type in common.AVAILABLE_TYPES:
-                    if os.path.exists(os.path.join(current_project, type)):
+def delete_projects(args):
+    current_project = os.path.join(common.PROJECT_LOCATION, args.name)
+    if os.path.isdir(current_project):
+        if args.type:
+            if args.type in common.AVAILABLE_TYPES:
+                current_project = os.path.join(current_project, args.type)
+                if os.path.exists(current_project):
+                    if os.path.exists(current_project):
                         shutil.rmtree(current_project)
+                else:
+                    print 'No project found of type: {0}'.format(args.type)
             else:
+                print 'Invalid type of project given to delete: {0}'.format(args.type)
+        else:
+            print 'delete project', current_project
+            if os.path.exists(current_project):
                 shutil.rmtree(current_project)
+            else:
+                print 'No such project found: {0}'.format(args.name)
+    else:
+        print 'No such project found: {0}'.format(args.name)
